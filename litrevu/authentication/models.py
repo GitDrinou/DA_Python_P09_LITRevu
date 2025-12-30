7a5bed6486e7010b2ee104cac2cd330126dc5fc1
Sandrine BRIVAL <brival.sandrine@gmail.com>
2025-12-30 13:56:39 +0100

    refactor(): delete User role field

--- litrevu/authentication/models.py
+++ litrevu/authentication/models.py
@@ -6,16 +6,4 @@ from litrevu.constants import USER_ROLE
 
 class User(AbstractUser):
     """User model"""
-    ADMIN = 'admin'
-    SUBSCRIBER = 'subscriber'
-
-    ROLE_CHOICES = (
-        (ADMIN, USER_ROLE['r_admin']),
-        (SUBSCRIBER, USER_ROLE['r_subscriber']),
-    )
-
-    role = models.CharField(
-        max_length=50,
-        choices=ROLE_CHOICES,
-        default=SUBSCRIBER,
-        verbose_name="Rôle")
+    pass
