f1ee9abe5e0815f2ee9a6e3d43e2ca5f3f6fe908
Sandrine BRIVAL <brival.sandrine@gmail.com>
2026-01-09 11:02:01 +0100

    refactor(): clean CSS

--- litrevu/authentication/forms.py
+++ litrevu/authentication/forms.py
@@ -1,9 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import MinLengthValidator
 
 
 class SignUpForm(UserCreationForm):
     """ Form for signing up users """
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ('username',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].validators.append(MinLengthValidator(6))
